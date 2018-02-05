86fdc79738b0e3047f9f8f4093b932aee6c2214e
davidexime <you@example.com>
2018-02-05 13:03:30 -0500

    first commit to hangman

---
+++ index.js
@@ -0,0 +1,177 @@
+var getRandomWordarry = ["pizza","chinese food","wings","Pho","griot"];
+var randomWord = randomWordArr[math.floor(Math.random() * randomWordArr.length]
+
+var s;
+var count = 0 ;
+var answerArray = [];
+
+
+function startup() 
+
+{
+	
+}
+ for  (var i = 0; i < randomWord.length; i++)
+ {
+	 answerArray[i] = "_";
+ }
+
+ s = answerArray.join (" ");
+ docu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var germancars = ['bmw', 'audi', 'porches','mercedes', 'volkswagen']
+// var  word = germancars[Math.floor(Math.random() * germancars.length)];
+
+// console.log(word)
+
+// // make input box and set
+													
+
+
+
+// for(let i=0;i<word.length;i++){
+// 	var div = document.createElement('DIV');
+// 	var letters = document.createTextNode(word[i])
+// 	div.appendChild(letters);
+// 	div.style.width='70px';
+// 	div.style.height='70px';
+// 	div.style.border='2px solid';
+// 	div.style.margin='10px';
+// 	div.style.display='inline-block';
+// 	div.classList.add('box');
+// 	document.body.appendChild(div);
+// 	var boxes = document.getElementsByClassName('box');
+// 	boxes[i].addEventListener('click', function(){
+// 				showLetter(i)
+// 	})
+// }
+
+// function showLetter(currentItem){
+// 	console.log(germancars[currentItem])
+// }
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+// var word = getRandomWord();
+
+// console.log(word);
+
+// document.getElementById("word").textContent = word;
+
+//   var words = [ "football", "basketball", "basketball", "hockey"
+//       ];
+
+//   var word = words[Math.floor(Math.random() * words.length)];
+
+//     // Set up the answer array
+//     var answerArray = [];
+//     for (var i = 0; i < word.length; i++) {
+//        answerArray[i] = "_";
+
+//     }
+
+// //     var remainingLetters = word.length;
+
+// //     // The game loop
+
+// //     while (remainingLetters > 0) {
+
+//       // Show the player their progress
+
+//       alert(answerArray.join(" "));
+
+//       // Get a guess from the player
+
+//       var guess = prompt("Guess a letter, or click Cancel to stop
+
+// playing.");
+
+//       if (guess === null) {
+
+//         // Exit the game loop
+
+//         break;
+
+//       } else if (guess.length !== 1) {
+
+//         alert("Please enter a single letter.");
+
+//     //   } else {
+
+//     //     // Update the game state with the guess
+
+//     //     for (var j = 0; j < word.length; j++) {
+
+//     //       if (word[j] === guess) {
+
+//     //         answerArray[j] = guess;
+
+//     //         remainingLetters--;
+
+//     //       }
+
+//     //     }
+
+//     //   }
+
+//     // // The end of the game loop
+
+    // }
+
+    // // Show the answer and congratulate the player
+
+    // alert(answerArray.join(" "));
+
+    // alert("Good job! The answer was " + word);
